7872d3ca8ac0d0010c7b02ff9021ddf28dda8227
filinils <filip.nilsson@capgemini.com>
2018-01-06 11:27:32 +0100

    inital config

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.clientSettings = {
+	authorizationURL: "http://localhost:3001/dialog/authorize",
+	tokenURL: "http://localhost:3001/oauth/token",
+	clientID: "<clientid>",
+	clientSecret: "<clientsecret>",
+	callbackURL: "http://localhost:3000/auth/provider/callback"
+};
